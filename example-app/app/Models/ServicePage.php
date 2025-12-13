f5ef2ba02ae78eeae56ca5c25e60b2f246917f10
Sumiyuru P Alwis <sumiyurupriyasara@gmail.com>
2025-12-13 12:15:11 +0530

    Update ServicePage.php

--- example-app/app/Models/ServicePage.php
+++ example-app/app/Models/ServicePage.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ServicePage extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'category',
+        'icon',
+        'content',
+        'thumbnail',
+        'video_url'
+    ];
 }
